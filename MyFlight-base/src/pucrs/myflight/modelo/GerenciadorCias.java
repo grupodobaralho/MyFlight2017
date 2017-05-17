b880dff7e8b07d167bea0cc37fde4ed24c57b873
Israel Deorce <israel.idvj@gmail.com>
2017-05-17 19:07:21 -0300

    Commit de aula EXE1 TEM Q FAZER

--- MyFlight-base/src/pucrs/myflight/modelo/GerenciadorCias.java
+++ MyFlight-base/src/pucrs/myflight/modelo/GerenciadorCias.java
@@ -1,13 +1,21 @@
 package pucrs.myflight.modelo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class GerenciadorCias {
 
 	private ArrayList<CiaAerea> empresas;
+	private ArrayList<String> listaEmpresas;
 
 	public GerenciadorCias() {
 		empresas = new ArrayList<>();
+		listaEmpresas = new ArrayList<>();
 	}
 
 	public void adicionar(CiaAerea cia) {
@@ -32,6 +40,24 @@ public class GerenciadorCias {
 				return a;
 		}
 		return null;
+	}
+
+	public void carregaDados() throws IOException {
+		Path path = Paths.get("Files/airlines.dat");
+		
+		try (BufferedReader reader = Files.newBufferedReader(path, Charset.forName("utf8"))) {
+			  String line = null;
+			 // reader.readLine()
+			  while ((line = reader.readLine()) != null) {
+			    System.out.println("Linha: "+line);
+			    //String[] dados = linha.split(";");
+			    //CiaAerea nova = new CiaAerea(dados[0], dados[1]);
+			  }
+			}
+			catch (IOException x) {
+			  System.err.format("Erro de E/S: %s%n", x);
+			}
+		
 	}
 
 }
